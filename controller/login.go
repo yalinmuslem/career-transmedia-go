83b5b7df29e116e1689a1c72ff544aa076d7426c
Myyalin <yalin.muslem@gmail.com>
2023-10-28 13:01:28 +0700

    UPDATE

--- controller/login.go
+++ controller/login.go
@@ -1,27 +1,40 @@
 package login
 
 import (
+	"fmt"
+	"net/http"
+	"os"
 	"time"
 	"transmediacareer/data"
 
+	"github.com/dchest/captcha"
 	"github.com/gin-gonic/gin"
 )
 
+func ShowCaptcha(c *gin.Context, cap string) {
+	// fmt.Println(cap)
+	http.Handle("/assets/captcha", captcha.Server(captcha.StdWidth, captcha.StdHeight))
+}
+
 func ShowLoginForm(c *gin.Context, transmedia data.JSONData) {
 
-	// Menampilkan halaman form login
+	captchaDir := "assets/captcha"
+	if err := os.MkdirAll(captchaDir, os.ModePerm); err != nil {
+		panic(err)
+	}
 
+	captcha.SetCustomStore(&CustomStore{
+		CaptchaDir: captchaDir,
+	})
+
+	// Menampilkan halaman form login
+	captchaID := captcha.New()
+	fmt.Printf("captchaID: %v\n", captchaID)
 	tahunSaatIni := time.Now().Year()
-	c.HTML(200, "login-form.html", gin.H{
-		"DeskripsiPerusahaan": transmedia.TRANSMEDIA.DeskripsiPerusahaan,
-		"AlamatPerusahaan":    transmedia.TRANSMEDIA.AlamatPerusahaan,
-		"Tahun":               tahunSaatIni,
-		"Phone":               transmedia.TRANSMEDIA.KONTAK.PHONE,
-		"Fax":                 transmedia.TRANSMEDIA.KONTAK.FAX,
-		"Twitter":             transmedia.TRANSMEDIA.SosialMedia.Twitter,
-		"Facebook":            transmedia.TRANSMEDIA.SosialMedia.Facebook,
-		"Youtube":             transmedia.TRANSMEDIA.SosialMedia.Youtube,
-		"Instagram":           transmedia.TRANSMEDIA.SosialMedia.Instagram,
+	c.HTML(200, "login-form", gin.H{
+		"captcha_id": captchaID,
+		"transmedia": transmedia.TRANSMEDIA,
+		"Tahun":      tahunSaatIni,
 	})
 }
 
@@ -32,4 +45,37 @@ func HandleLogin(c *gin.Context) {
 
 	// Mengarahkan pengguna setelah login berhasil
 	c.Redirect(302, "/dashboard")
+}
+
+// CustomStore adalah penyimpanan CAPTCHA kustom
+type CustomStore struct {
+	CaptchaDir string
+}
+
+// Set mengatur CAPTCHA dengan ID ke dalam penyimpanan
+func (s *CustomStore) Set(id string, digits []byte) {
+	// Simpan gambar CAPTCHA ke dalam direktori CaptchaDir
+	filePath := s.CaptchaDir + "/" + id + ".png"
+	os.WriteFile(filePath, digits, 0644)
+	// captcha.WriteImage()
+}
+
+// Get mengambil gambar CAPTCHA dengan ID dari penyimpanan
+func (s *CustomStore) Get(id string, clear bool) (digits []byte) {
+	filePath := s.CaptchaDir + "/" + id + ".png"
+	digits, err := os.ReadFile(filePath)
+	if err != nil {
+		panic(err)
+	}
+	if clear {
+		_ = os.Remove(filePath)
+	}
+	return digits
+}
+
+// Clear menghapus CAPTCHA dengan ID dari penyimpanan
+func (s *CustomStore) Clear(id string) {
+	// Hapus gambar CAPTCHA dari direktori CaptchaDir
+	filePath := s.CaptchaDir + "/" + id + ".png"
+	_ = os.Remove(filePath)
 }
--- main.go
+++ main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"encoding/json"
 	"os"
+
 	"path/filepath"
+
 	login "transmediacareer/controller"
 	"transmediacareer/data"
 
+	"github.com/gin-gonic/contrib/renders/multitemplate"
 	"github.com/gin-gonic/gin"
 )
 
@@ -29,19 +32,31 @@ func main() {
 		panic(err)
 	}
 
-	r.LoadHTMLGlob("templates/*")
+	r.HTMLRender = loadTemplates("./templates")
 	r.Static("/assets", "assets")
 
-	login := login.ShowLoginForm
 	transmedia := jsonData
 
 	r.GET("/login", func(ctx *gin.Context) {
 
-		login(ctx, transmedia)
+		login.ShowLoginForm(ctx, transmedia)
 	})
 	r.POST("/login", func(ctx *gin.Context) {
 		// login.HandleLogin(ctx)
 	})
-
+	r.GET("/captcha/:captcha", func(ctx *gin.Context) {
+		captchaID := ctx.Param("captcha")
+		login.ShowCaptcha(ctx, captchaID)
+	})
 	r.Run()
+}
+
+func loadTemplates(templatesDir string) multitemplate.Render {
+	r := multitemplate.New()
+
+	r.AddFromFiles("login-form",
+		templatesDir+"/user/login/login-form.html",
+		templatesDir+"/user/login/content.html",
+		templatesDir+"/user/menu.html")
+	return r
 }
